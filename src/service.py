60cbba72a83c0ebf1e5f2296d0b06df31bc60bcb
Goalina <15861868579@163.com>
2025-03-28 16:37:50 +0800

    feat: 后端补全 添加dockerfile (#9)
    /LTGM

--- src/service.py
+++ src/service.py
@@ -1,11 +1,14 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-import subprocess
-import re
+from obs_download import download_file
+from upload import upload
+from method import TranscriptionProcessor
+
 import os
 import configparser
-
-from .obs_download import download_file
+import subprocess
+from venv import logger
+import requests
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 config = configparser.ConfigParser()
 config.read("/app/config.ini")
@@ -14,18 +17,24 @@ download_directory = config["paths"]["download_directory"].replace('"', "").stri
 output_directory = config["paths"]["output_directory"].replace('"', "").strip()
 hf_token = config["token"]["hf_token"].replace('"', "").strip()
 
+callback_url = config["callback"]["url"].replace('"', "").strip()
+storage_server = config["storage"]["server"].replace('"', "").strip()
+storage_bucket = config["storage"]["bucket"].replace('"', "").strip()
+
 app = FastAPI()
 
 
 class DownloadRequest(BaseModel):
     object_key: str
     bucket_key: str
+    mid: str
 
 
 @app.post("/meeting_translate")
 async def meeting_translate(request: DownloadRequest):
     object_key = request.object_key
     bucket_key = request.bucket_key
+    mid = request.mid
     download_path = os.path.join(download_directory, object_key.split("/")[-1])
 
     result = download_file(bucket_key, object_key, download_path)
@@ -33,8 +42,11 @@ async def meeting_translate(request: DownloadRequest):
     if not result["success"]:
         raise HTTPException(status_code=500, detail=result["errorMessage"])
 
-    output_file = os.path.join(
-        output_directory, f"{os.path.splitext(os.path.basename(download_path))[0]}.srt"
+    vtt_file = os.path.join(
+        output_directory, f"{os.path.splitext(os.path.basename(download_path))[0]}.vtt"
+    )
+    json_file = os.path.join(
+        output_directory, f"{os.path.splitext(os.path.basename(download_path))[0]}.json"
     )
 
     try:
@@ -60,17 +72,44 @@ async def meeting_translate(request: DownloadRequest):
             "20",
             "--suppress_tokens",
             "-1",
+            "--max_line_width",
+            "20",
         ]
 
-        process = subprocess.run(whisperx_command, capture_output=True, text=True)
+        process = subprocess.run(
+            whisperx_command, capture_output=True, text=True, shell=False
+        )
 
         if process.returncode != 0:
             raise HTTPException(status_code=500, detail=process.stderr)
 
-        with open(output_file, "r", encoding="utf-8") as f:
+        with open(vtt_file, "r", encoding="utf-8") as f:
             transcription_text = f.read()
 
-        adjusted_transcription = adjust_speaker_numbers(transcription_text)
+        formater = TranscriptionProcessor()
+
+        adjusted_transcription = formater.adjust_speaker_numbers(transcription_text)
+
+        with open(vtt_file, "w", encoding="utf-8") as f:
+            f.write(adjusted_transcription)
+
+        formater.convert_vtt_to_json(vtt_file)
+
+        vtt_object_key, json_object_key = formater.generate_object_keys(
+            mid, object_key, vtt_file, json_file
+        )
+
+        upload(
+            storage_server, storage_bucket, file_path=vtt_file, objectKey=vtt_object_key
+        )
+        upload(
+            storage_server,
+            storage_bucket,
+            file_path=json_file,
+            objectKey=json_object_key,
+        )
+
+        send_callback(mid, vtt_path=vtt_object_key, json_path=json_object_key)
 
         return {
             "message": "Transcription succeeded",
@@ -81,19 +120,33 @@ async def meeting_translate(request: DownloadRequest):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-def adjust_speaker_numbers(transcription):
-    speaker_count = 1
-    speaker_map = {}
-    speaker_pattern = re.compile(r"\[SPEAKER_(\d+)\]")
+def send_callback(mid: str, vtt_path: str, json_path: str):
+    """异步发送回调请求"""
+    try:
+        response = requests.post(
+            callback_url,
+            json={
+                "mid": mid,
+                "text_vtt_url": f"https://{storage_bucket}.{storage_server}/{vtt_path}",
+                "text_json_url": f"https://{storage_bucket}.{storage_server}/{json_path}",
+            },
+        )
 
-    def replace_speaker(match):
-        nonlocal speaker_count
-        speaker = match.group(1)
-        if speaker not in speaker_map:
-            speaker_map[speaker] = f"SPEAKER_{speaker_count:02d}"
-            speaker_count += 1
-        return f"[{speaker_map[speaker]}]"
+        if response.status_code == 200:
+            logger.info("Callback succeeded: %s", response.json())
+        else:
+            logger.error(
+                "Callback failed for mid: %s, status code: %d, response: %s",
+                mid,
+                response.status_code,
+                response.text,
+            )
 
-    adjusted_transcription = speaker_pattern.sub(replace_speaker, transcription)
+    except Exception as e:
+        logger.error("Callback failed for mid: %s, error: %s", mid, str(e))
 
-    return adjusted_transcription
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
